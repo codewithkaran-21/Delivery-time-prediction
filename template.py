ded4e58a5637603b0232adc3a2c5584fefa62b98
Karan Singh <godthatback21@gmail.com>
2025-09-18 18:59:28 +0530

    project template created successfully

---
+++ template.py
@@ -0,0 +1,39 @@
+import os , sys
+from pathlib import Path
+import logging
+
+while True:
+    project_name = input("Enter the name of the project : ")
+    if project_name!="":
+        break
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/config__init__.py",
+    f"{project_name}/constants__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline__init__.py",
+    f"{project_name}/utils/__init__.py"
+    f"config/config.yaml",
+    f"schema.yaml",
+    f"main.py",
+    f"setup.py",
+    f"requirements.txt",
+    f"app.py",
+    f"logs.py",
+    f"exception.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir , filename = os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir , exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath , "w") as f:
+            pass
+        
